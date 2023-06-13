3bfc479ce75f6b311cf08c1fab3df6387cdf58fd
Nathan-Hausman <hausmann@oregonstate.edu>
2023-06-13 13:54:45 -0700

    Initial implementation of authentication middleware

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken')
+const { User } = require('../models/user.model')
+
+const secretKey = 'placeholder'
+
+function genToken(userId) {
+    const payload = { sub: userId }
+    return jwt.sign(payload, secretKey, { expiresIn: '24h' })
+}
+
+async function requireAuth(req, res, next) {
+    const authHeader = req.get('Authorization') || ''
+    const authHeaderParts = authHeader.split(' ')
+    const token = authHeaderParts[0] == 'Bearer' ? authHeaderParts[1] : null
+
+    try {
+        if (token) {
+            const payload = jwt.verify(token, secretKey)
+            req.user = payload.sub
+            const user = await User.findByPk(payload.sub)
+            req.isAdmin = user.admin
+        } else {
+            req.user = null
+            req.isAdmin = false
+        }
+        next()
+    } catch (e) {
+        res.status(401).json({ error: "Invalid authentication token.", msg: e.message })
+    }
+}
+
+exports.genToken = genToken
+exports.requireAuth = requireAuth
